01f0bbcf5b692d274de75ca0071f92a8791cc78f
Sophie Axtens <2856270A@student.gla.ac.uk>
2023-02-09 14:04:45 +0000

    Commit for Chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -29,9 +29,11 @@ def populate():
         {'title':'Flask',
         'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages} }
+    cats = {
+    'Python' : {'pages': python_pages, 'views':128, 'likes':64},
+    'Django': {'pages': django_pages, 'views':64, 'likes':32},
+    "Other Frameworks": {'pages':other_pages, 'views':32, 'likes':16},
+    }
 
 
     for cat, cat_data in cats.items():
@@ -50,10 +52,12 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
-    c.save()
+    c.views=views
+    c.likes=likes
     return c
+
 
 if __name__ == '__main__':
     print('Starting Rango population script...')
