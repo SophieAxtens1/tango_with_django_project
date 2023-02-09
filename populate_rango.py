884eb6cecd717f647bde390fa87504dd62207e15
Sophie Axtens <2856270A@student.gla.ac.uk>
2023-02-09 21:49:13 +0000

    Test Commit for Chapter 5, views are set to 128.

--- populate_rango.py
+++ populate_rango.py
@@ -30,9 +30,9 @@ def populate():
         'url':'http://flask.pocoo.org'} ]
 
     cats = {
-    'Python' : {'pages': python_pages, 'views':128, 'likes':64},
-    'Django': {'pages': django_pages, 'views':64, 'likes':32},
-    "Other Frameworks": {'pages':other_pages, 'views':32, 'likes':16},
+    'Python': {'pages': python_pages,'views':128,'likes':64},
+    'Django': {'pages': django_pages,'views':64,'likes':32},
+    "Other Frameworks": {'pages':other_pages,'views':32,'likes':16},
     }
 
 
