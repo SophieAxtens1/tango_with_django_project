98d2d9f10dde9081a403c4dcf921109e8e70cf8f
Sophie Axtens <2856270A@student.gla.ac.uk>
2023-02-10 16:49:09 +0000

    Test Commit for Chapter 10, visit count not read correctly.

--- rango/views.py
+++ rango/views.py
@@ -21,20 +21,18 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    #context_dict['extra'] = 'From the model solution on GitHub'
+    context_dict['extra'] = 'From the model solution on GitHub'
 
     #request.session.set_test_cookie()
-
-    response = render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request, response)
-    return response
+    visitor_cookie_handler(request)
+    return render(request, 'rango/index.html', context=context_dict)
+    
 
 def about(request):
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
-
-    return render(request, 'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 
@@ -163,19 +161,16 @@ def get_server_side_cookie(request, cookie, default_val=None):
         val = default_val
     return val
 
-def visitor_cookie_handler(request, response):
+def visitor_cookie_handler(request):
     visits = int(request.COOKIES.get('visits', '1'))
     last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-    '%Y-%m-%d %H:%M:%S')
-    # If it's been more than a day since the last visit...
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    
+
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        # Update the last visit cookie now that we have updated the count
         request.session['last_visit'] = str(datetime.now())
-
     else:
-        # Set the last visit cookie
         request.session['last_visit'] = last_visit_cookie
-        # Update/set the visits cookie
-        request.session['visits'] = visits
+
+    request.session['visits'] = visits
