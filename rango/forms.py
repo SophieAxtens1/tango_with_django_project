8f2cd543e9b4010f6ba0e956fb0d05d374322c66
Sophie Axtens <2856270A@student.gla.ac.uk>
2023-02-10 14:46:18 +0000

    Commit for Chapter 8

--- rango/forms.py
+++ rango/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
-    help_text="Please enter the category name.")
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -15,24 +14,14 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-    help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-    help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         # Provide an association between the ModelForm and a mode
         model = Page
-
-# What fields do we want to include in our form?
-# This way we don't need every field in the model present.
-# Some fields may allow NULL values; we may not want to include them.
-# Here, we are hiding the foreign key.
-# we can either exclude the category field from the form,
         exclude = ('category',)
-# or specify the fields to include (don't include the category field).
-# fields = ('title', 'url', 'views')
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -42,4 +31,4 @@ class PageForm(forms.ModelForm):
         if url and not url.startswith('http://'):
             url = f'http://{url}'
             cleaned_data['url'] = url
-            return cleaned_data
+        return cleaned_data
